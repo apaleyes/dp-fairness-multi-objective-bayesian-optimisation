20ca606b9f04a05f891cb1bf3b5acf72a4151d73
Andrei Paleyes <apalees@gmail.com>
2023-01-31 16:03:34 +0000

    removed jobs defs

--- schedule-jobs/automated_experiment_2_PFLR/schedule_cpu.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd ~
-chmod +x /home/bf319/dp-fairness-multi-objective-bayesian-optimisation/pfairdp/automated_experiment_2_PFLR.py
-sbatch /home/bf319/dp-fairness-multi-objective-bayesian-optimisation/schedule-jobs/automated_experiment_2_PFLR/slurm_cpu.peta4-icelake
